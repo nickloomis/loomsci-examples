c62d7e5e99598f05e438d916575405285f713034
Nick Loomis <nloomis@gmail.com>
2019-01-20 22:06:06 -0800

    Sine drawings embedded into a class

--- python/sineimage.py
+++ python/sineimage.py
@@ -11,33 +11,41 @@ import cv2utils
 import matplotlib.pyplot as plt
 import numpy as np
 
+class Plotter(object):
+  def __init__(self, filename, num_strips):
+    self.source_image = cv2utils.imread(filename, cv2.IMREAD_GRAYSCALE)
+    self.source_height, self.source_width = self.source_image.shape
+    self.num_strips = int(num_strips)
+    self.strip_height = self.source_height / self.num_strips
 
-def reshape_source(filename, num_rows_final):
-  source_image = cv2utils.imread(filename, cv2.IMREAD_GRAYSCALE)
-  height, width = source_image.shape
-  scale = float(num_rows_final) / height
-  strip_height = height / num_rows_final
-  low_res_width = scale * width
-  low_res_image = cv2.resize(source_image, (int(low_res_width), int(num_rows_final)), interpolation=cv2.INTER_AREA);
-  reshaped_image = cv2.resize(low_res_image, (width, int(num_rows_final)), interpolation=cv2.INTER_CUBIC);
-  return reshaped_image, strip_height;
+  def _reshape_source(self):
+    scale = float(self.num_strips) / self.source_height
+    low_res_width = scale * self.source_width
+    low_res_image = cv2.resize(self.source_image, (int(low_res_width), self.num_strips), interpolation=cv2.INTER_AREA);
+    reshaped_image = cv2.resize(low_res_image, (self.source_width, self.num_strips), interpolation=cv2.INTER_CUBIC);
+    return reshaped_image
 
-def strip_center_y(num_rows, strip_height):
-  return np.linspace(0.5 * strip_height, (num_rows - 0.5) * strip_height, num_rows)
+  def _strip_center_y(self):
+    return np.linspace(0.5 * self.strip_height, (self.num_strips - 0.5) * self.strip_height, self.num_strips)
 
-def fast_gamma(reshaped_image):
-  return np.power(reshaped_image / 255, 2.2) * 255
+  @classmethod
+  def _fast_gamma(cls, reshaped_image):
+    return np.power(reshaped_image / 255, 2.2) * 255
 
-def draw_sinusoids(reshaped_image, strip_height):
-  num_rows, width = reshaped_image.shape
-  y_centers = strip_center_y(num_rows, strip_height)
-  x = np.linspace(0, width - 1, width)
-  omega = 0.5
-  intensity_image = fast_gamma(reshaped_image)
-  for i in range(num_rows):
-    amplitude = strip_height * 0.5 * intensity_image[i, :] / 255
-    plt.plot(x, amplitude * np.sin(omega * x) + y_centers[i], color='black')
-  plt.box(False)
-  plt.show()
+  def draw(self):
+    reshaped_image = self._reshape_source()
+    num_rows, width = reshaped_image.shape
+    y_centers = self._strip_center_y()
+    x = np.linspace(0, width - 1, width)
+    omega = 0.5
+    intensity_image = self._fast_gamma(reshaped_image)
+    for i in range(num_rows):
+      amplitude = self.strip_height * 0.5 * intensity_image[i, :] / 255
+      plt.plot(x, amplitude * np.sin(omega * x) + y_centers[i], color='black')
+    plt.box(False)
+    plt.show()
 
-# TODO(nloomis): convert sRGB grayscale->linear intensity
+# TODO(nloomis): flipud on graph
+# TODO(nloomis): option to scale to max intensity
+# TODO(nloomis): save out image
+# TODO(nloomis): options for diff't color scheme
